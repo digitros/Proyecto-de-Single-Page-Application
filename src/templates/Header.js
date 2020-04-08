b3580ec4165e244629bcb9882ad7efeda436ddb8
Diego Valdez Acosta <dgogo9@hotmail.com>
2020-04-08 15:51:36 -0500

    Proyecto de Single Page Application

---
+++ src/templates/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+    const view = `
+        <div class="Header-main">
+            <div class="Header-logo">
+                <h1>
+                    <a hfer="/">
+                        100tifi.co
+                    </a>
+                </h1>
+            </div>
+            <div>
+                <a href="#/about/">About</a>
+            </div>
+        </div>
+    `;
+    return view;
+}
+
+export default Header
